c08327082ceae040289c6bebe1dd69cc598b1140
seanclarkeguardian <sean.clarke@guardian.co.uk>
2018-07-09 15:01:06 +0100

    remove placeholder text

--- src/render.js
+++ src/render.js
@@ -4,5 +4,5 @@ export async function render() {
     // this function just has to return a string of HTML
     // you can generate this using js, e.g. using Mustache.js
 
-    return templateHTML;
+    return '';
 }
